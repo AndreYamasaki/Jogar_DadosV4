35c8703698c978be98e1f9b5df84c1543db8b038
RangelBeatriz <srangel.beatriz@gmail.com>
2021-05-06 23:40:05 -0400

    Creating storyboard layout

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -8,10 +8,15 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var dadoImageView1: UIImageView!
+    @IBOutlet weak var dadoImageView2: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        dadoImageView1.image = #imageLiteral(resourceName: "DiceOne")
+        dadoImageView1.alpha = 0.5
     }
 
 
