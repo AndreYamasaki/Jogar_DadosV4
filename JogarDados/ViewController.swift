0d840484e07d5cf84f6ad479599b230427de3722
RangelBeatriz <srangel.beatriz@gmail.com>
2021-05-10 17:35:45 -0400

    Adding functionality to Rolar Dados button

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -9,16 +9,27 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    //MARK: - Outlets
     @IBOutlet weak var dadoImageView1: UIImageView!
     @IBOutlet weak var dadoImageView2: UIImageView!
+    var dice = [UIImage(named: "DiceOne"), UIImage(named: "DiceTwo"), UIImage(named: "DiceThree"), UIImage(named: "DiceFour"), UIImage(named: "DiceFive"), UIImage(named: "DiceSix")]
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        dadoImageView1.image = #imageLiteral(resourceName: "DiceOne")
-        dadoImageView1.alpha = 0.5
+        dadoImageView1.image = UIImage(named: "DiceOne")
+        dadoImageView2.image = UIImage(named: "DiceTwo")
     }
+    
+    
+    //MARK: -Actions
+    @IBAction func rolarDadosPressed(_ sender: UIButton) {
+        
+        (dadoImageView1.image, dadoImageView2.image) = (dice.randomElement()!, dice.randomElement()!)
 
-
+        
+    }
+    
 }
 
