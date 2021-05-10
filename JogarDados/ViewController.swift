33565ac86bbaac4f8685dc2c2199e53e3641636e
AndreYamasaki <katsuya.tsu@gmail.com>
2021-05-10 17:13:39 -0400

    Adicionado array com imagens aos dados e action aleatorio botão

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -7,20 +7,26 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
     
-     //MARK: - IBOutlet
+     //MARK: - Outlet
     @IBOutlet weak var dadoImageViewOne: UIImageView!
     @IBOutlet weak var dadoImageViewTwo: UIImageView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        dadoImageViewOne.image = #imageLiteral(resourceName: "DiceFour")
-        dadoImageViewOne.alpha = 0.5
         // Do any additional setup after loading the view.
     }
+    
+//MARK: - Actions
 
-
+    @IBAction func rolarDadoPressed(_ sender: UIButton) {
+        
+        dadoImageViewOne.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
+        dadoImageViewTwo.image = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")][Int.random(in: 0...5)]
+        
 }
 
+}
