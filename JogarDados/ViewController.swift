650b06b3870b062e8629f9e5e405189d89626626
karinamsilva <karinamartinssilva@live.com>
2021-05-10 14:33:21 -0700

    rolando dados randoômicamente

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -14,8 +14,7 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var dadoImageView2: UIImageView!
     
-    var selectDado1 =  0
-    var selectDado2 = 5
+
     
     
     override func viewDidLoad() {
@@ -23,19 +22,16 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view.
         
     }
+    
+    //MARK: - Actions
     @IBAction func rolarDadosPressed(
         sender: UIButton){
-        dadoImageView1.image =  [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][selectDado1]
-        selectDado1+=1
+        let dados = [UIImage(named:"DiceOne"),UIImage(named:"DiceTwo"),UIImage(named:"DiceThree"),UIImage(named:"DiceFour"),UIImage(named:"DiceFive"),UIImage(named:"DiceSix")]
     
-        dadoImageView2.image =  [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][selectDado2]
-        selectDado2-=1
-      
-        if(selectDado2 < 0){
-            selectDado2 = 5
-        }
-        if(selectDado1 > 5){
-            selectDado1 = 0        }
+        
+        dadoImageView1.image = dados[Int.random(in: 0...5)]
+        
+        dadoImageView2.image = dados[Int.random(in: 0...5)]
         
     }
 
