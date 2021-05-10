7d0658e2c66d64a736ebb7a2dcbb8e0c47606724
karinamsilva <karinamartinssilva@live.com>
2021-05-10 13:03:50 -0700

    adicionado acções ao botão rolar dados

--- JogarDados/ViewController.swift
+++ JogarDados/ViewController.swift
@@ -14,17 +14,30 @@ class ViewController: UIViewController {
     
     @IBOutlet weak var dadoImageView2: UIImageView!
     
+    var selectDado1 =  0
+    var selectDado2 = 5
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
         
-        dadoImageView1.image = #imageLiteral(resourceName: "DiceFour")
-        
-        
-        
+    }
+    @IBAction func rolarDadosPressed(
+        sender: UIButton){
+        dadoImageView1.image =  [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][selectDado1]
+        selectDado1+=1
+    
+        dadoImageView2.image =  [#imageLiteral(resourceName: "DiceOne"),#imageLiteral(resourceName: "DiceTwo"),#imageLiteral(resourceName: "DiceThree"),#imageLiteral(resourceName: "DiceFour"),#imageLiteral(resourceName: "DiceFive"),#imageLiteral(resourceName: "DiceSix")][selectDado2]
+        selectDado2-=1
+      
+        if(selectDado2 < 0){
+            selectDado2 = 5
+        }
+        if(selectDado1 > 5){
+            selectDado1 = 0        }
         
     }
-
 
 }
 
